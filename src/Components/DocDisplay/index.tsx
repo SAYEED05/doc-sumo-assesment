8de9a790ab759a3d39d8b19f1938e4fded0a99d8
sayeedzh <sayeed.705@gmail.com>
2024-05-16 21:37:27 +0530

    wip

--- src/Components/DocDisplay/index.tsx
+++ src/Components/DocDisplay/index.tsx
@@ -13,10 +13,13 @@ const DocDisplay = () => {
       return {
         position: item?.content?.position,
         value: item?.content?.value,
+        id: item?.id,
       };
     });
     return _modified;
   }, [dataToSubmit]);
+
+  console.log(positionMapper, "positionMapper");
 
   return (
     <Grid
@@ -37,7 +40,7 @@ const DocDisplay = () => {
       >
         <img src={image} alt="document" loading="lazy" />
         {positionMapper?.map((rectangle: any) => (
-          <HighlightRectangle position={rectangle} />
+          <HighlightRectangle key={rectangle.id} position={rectangle} />
         ))}
       </Grid>
     </Grid>
